e397c9f16718682f92efc14cfb71b45a9a52875b
Sambo Chea <sombochea@cubetiqs.com>
2022-09-12 19:11:41 +0700

    Add custom build and updated the bimg

--- version.go
+++ version.go
@@ -3,9 +3,12 @@ package main
 // Version stores the current package semantic version
 var Version = "dev"
 
+var CustomBuild = "cubetiqs.com"
+
 // Versions represents the used versions for several significant dependencies
 type Versions struct {
 	ImaginaryVersion string `json:"imaginary"`
 	BimgVersion      string `json:"bimg"`
 	VipsVersion      string `json:"libvips"`
+	CustomBuild      string `json:"custom_build"`
 }
